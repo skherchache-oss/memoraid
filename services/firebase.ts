58688013c341863a3f856c78dc42d52fa81f79a4
Salima Kherchache <salima.kherchache@inrae.fr>
2025-12-31 02:16:23 +0100

    ${npm_package_version}

--- services/firebase.ts
+++ services/firebase.ts
@@ -9,7 +9,7 @@ const apiKey = (process.env as any).VITE_FIREBASE_API_KEY || (import.meta as any
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: "memoraid-app.fr",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
