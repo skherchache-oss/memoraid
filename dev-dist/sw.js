4df531e2f892ccc8443685210532516b42390504
Salima Kherchache <salima.kherchache@inrae.fr>
2026-01-09 19:02:27 +0100

    ${npm_package_version}

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.an1un16abtc"
+    "revision": "0.6miq8561p9k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
